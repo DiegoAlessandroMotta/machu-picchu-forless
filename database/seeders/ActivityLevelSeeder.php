c96e472b836168adabbdf623709caae203b4e998
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-03-12 04:59:03 -0500

    Fix: Add seeders

---
+++ database/seeders/ActivityLevelSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ActivityLevels;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
+
+class ActivityLevelSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    if (App::environment('production')) {
+      ActivityLevels::insert([
+        [
+          'code' => 'easy',
+          'name' => 'Easy',
+        ],
+        [
+          'code' => 'moderate',
+          'name' => 'Moderate',
+        ],
+        [
+          'code' => 'mid',
+          'name' => 'Mid',
+        ],
+        [
+          'code' => 'hard',
+          'name' => 'Hard',
+        ],
+        [
+          'code' => 'challenging',
+          'name' => 'Challenging',
+        ],
+      ]);
+    }
+  }
+}
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TourCategories;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
+
+class CategorySeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    if (App::environment('production')) {
+      TourCategories::insert([
+        [
+          'code' => 'uncategorized',
+          'name' => 'Uncategorized',
+        ],
+      ]);
+    }
+  }
+}
