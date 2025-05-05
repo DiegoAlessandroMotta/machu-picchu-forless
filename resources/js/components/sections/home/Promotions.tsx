aaba6d2cc82f8c051a4a74f4a400be4473abeaa4
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-05-05 14:59:40 -0500

    Refactor: update home page

--- resources/js/components/sections/home/Promotions.tsx
+++ resources/js/components/sections/home/Promotions.tsx
@@ -1,56 +1,39 @@
 import { BorderButton } from '@/components/atoms/BorderButton'
 import { PackageCard } from '@/components/ui/PackageCard'
-// import toursData from '@/mocks/tours.json'
-import { chunks, truncateText } from '@/utils/utils'
+import { truncateText } from '@/utils/utils'
 import { Link } from '@inertiajs/react'
 
 interface Props {
 	tours: Tour[]
 }
 
-export const Promotions = ({ tours }: Props) => {
-	const chunkedTours = chunks(tours, 3)
-
+export const Promotions = ({ tours: promotions }: Props) => {
 	return (
 		<section className="px-4 md:px-8">
-			<div className="mx-auto grid max-w-layout grid-cols-1 gap-4 pt-16 lg:grid-cols-2">
-				{chunkedTours.map((promotions, index) => {
-					const colSpan2Start = index % 2 == 0
-
-					return (
-						<div className="grid gap-4 md:grid-cols-2" key={promotions[0].id}>
-							{promotions.map((promotion, index) => {
-								const style = 'md:col-span-2'
-								const className =
-									(colSpan2Start && index == 0) ||
-									(!colSpan2Start && index == promotions.length - 1)
-										? style
-										: ''
-
-								return (
-									<Link
-										href={`/tour/${promotion.code}`}
-										className="contents"
-										prefetch
-										key={promotion.id}
-									>
-										<PackageCard
-											title={promotion.name}
-											days={promotion.days}
-											price={promotion.price}
-											description={truncateText(
-												promotion.description ?? '',
-												10
-											)}
-											bgImgUrl={promotion.main_banner}
-											className={className}
-										/>
-									</Link>
-								)
-							})}
-						</div>
-					)
-				})}
+			<div className="mx-auto flex max-w-layout flex-col gap-4 pt-16">
+				<div
+					className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3"
+					key={promotions[0].id}
+				>
+					{promotions.map((promotion) => {
+						return (
+							<Link
+								href={`/tour/${promotion.code}`}
+								className="contents"
+								prefetch
+								key={promotion.id}
+							>
+								<PackageCard
+									title={promotion.name}
+									days={promotion.days}
+									price={promotion.price}
+									description={truncateText(promotion.description ?? '', 10)}
+									bgImgUrl={promotion.main_banner}
+								/>
+							</Link>
+						)
+					})}
+				</div>
 			</div>
 
 			<div className="mt-16 flex justify-center">
--- resources/js/components/ui/PackageCard.tsx
+++ resources/js/components/ui/PackageCard.tsx
@@ -1,10 +1,11 @@
+import { truncateText } from '@/utils/utils'
+
 interface Props {
 	title: string
 	price: string
 	days: number
 	description: string
 	bgImgUrl: string
-	className?: string
 }
 
 export const PackageCard = ({
@@ -12,30 +13,30 @@ export const PackageCard = ({
 	price,
 	days,
 	description,
-	bgImgUrl,
-	className = ''
+	bgImgUrl
 }: Props) => {
-	return (
-		<article
-			className={`relative flex h-80 items-stretch overflow-hidden rounded-md shadow-md lg:h-96 ${className}`}
-		>
-			<div
-				className="w-full overflow-x-hidden"
-				style={{
-					background: `linear-gradient(180deg, #027A7C 0%, rgba(0, 0, 0, 0.00) 80%), url(${bgImgUrl}) lightgray 50% / cover no-repeat`
-				}}
-			></div>
+	const truncatedDescription = truncateText(description, 15)
 
-			<div className="ml-[-100%] flex w-full flex-col items-center gap-4 px-3 py-6 text-center text-white">
-				<h3 className="text-2xl font-bold uppercase lg:text-3xl">{title}</h3>
-				<div className="flex items-center gap-2">
-					<span className="rounded-full bg-primary px-4 py-1 font-semibold">
+	return (
+		<article className="flex h-80 items-stretch overflow-hidden rounded-md shadow-md lg:h-96">
+			<div
+				className="flex w-full flex-col items-center gap-4 px-3 py-6 text-center text-white"
+				style={{
+					background: `linear-gradient(180deg, #027A7C 0%, rgba(0, 0, 0, 0.00) 70%), url(${bgImgUrl}) #dddddd 50% / cover no-repeat`
+				}}
+			>
+				<h3 className="text-2xl font-bold uppercase">{title}</h3>
+				<div className="flex flex-wrap items-center justify-center gap-2">
+					<span className="text-nowrap rounded-full bg-primary px-4 py-1 font-medium">
 						{days} Days
 					</span>
 					<span>From ${price}</span>
 				</div>
-				<p className="text-lg">{description}</p>
+				<p>{truncatedDescription}</p>
 			</div>
+			{/* <div className="flex justify-end">
+				<button>View tour {'->'}</button>
+			</div> */}
 		</article>
 	)
 }
